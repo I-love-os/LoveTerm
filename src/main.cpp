3911d1f55af25e25107ea877420324069a254e1f
yknomeh <yknomeh@protonmail.com>
2018-11-07 06:36:20 +0100

    copy && paste preparations

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 
 const gchar TERM_NAME[9] = "LoveTerm";
 
+gboolean key_is_pressed(GtkWidget *widget, GdkEvent *event, gpointer data) {
+     VteTerminal *term = VTE_TERMINAL(widget);
+     GtkWidget *win = (GtkWidget *)data;
+
+     if (((GdkEventKey *)event)->state & GDK_CONTROL_MASK) {
+         switch (((GdkEventKey *)event)->keyval) {
+             case GDK_KEY_C:
+                 vte_terminal_copy_clipboard_format(term, VTE_FORMAT_TEXT);
+                 return TRUE;
+             case GDK_KEY_V:
+                 vte_terminal_paste_clipboard(term);
+                 return TRUE;
+             default:
+                 return FALSE;
+         }
+    }
+}
 int main(int argc, char **argv) {
 
     gtk_init(&argc, &argv);
@@ -37,6 +54,7 @@ int main(int argc, char **argv) {
 
     g_signal_connect(window, "delete-event", gtk_main_quit, NULL);
     g_signal_connect(terminal, "child-exited", gtk_main_quit, NULL);
+    g_signal_connect(terminal, "key-press_event", G_CALLBACK(key_is_pressed), vte);
 
     gtk_container_add(GTK_CONTAINER(window), vte);
     gtk_widget_show_all(window);
